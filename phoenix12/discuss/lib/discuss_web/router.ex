2c84f1f3d0ced72675129c0767999a79fe044afe
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-06 00:29:35 -0500

    discuss: start work on topic controller

--- phoenix12/discuss/lib/discuss_web/router.ex
+++ phoenix12/discuss/lib/discuss_web/router.ex
@@ -18,6 +18,8 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/topics/new", TopicController, :new
+    # get "/topics/"
   end
 
   # Other scopes may use custom stacks.
