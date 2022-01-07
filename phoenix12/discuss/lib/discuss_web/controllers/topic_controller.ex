d41bae4431625267ad7ada6a675a3722e86835ee
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-07 00:30:56 -0500

    discuss: implement deleting topics

--- phoenix12/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ phoenix12/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -47,4 +47,14 @@ defmodule DiscussWeb.TopicController do
         render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Topic
+    |> Repo.get!(topic_id)
+    |> Repo.delete!()
+
+    conn
+    |> put_flash(:info, "Topic deleted!")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
