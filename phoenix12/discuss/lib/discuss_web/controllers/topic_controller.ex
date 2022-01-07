908a0f8c262afadb108143e4efa7be6656560d14
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-06 22:49:40 -0500

    discuss: use flash and redirect on topic creation

--- phoenix12/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ phoenix12/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -12,7 +12,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, _post} ->
+        conn
+        |> put_flash(:info, "Topic created!")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} -> render conn, "new.html", changeset: changeset
     end
   end
