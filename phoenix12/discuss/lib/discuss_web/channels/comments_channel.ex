cb14627858f3466c44018b8c9f70f037007d3ff7
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-08 20:46:50 -0500

    discuss: initial websocket comment posting

--- phoenix12/discuss/lib/discuss_web/channels/comments_channel.ex
+++ phoenix12/discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,28 +1,34 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.Topic
+  alias Discuss.Repo
+  alias Discuss.Comment
+  import Ecto
 
   @impl true
-  def join("comments:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
+  def join("comments:" <> topic_id, payload, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Topic
+    |> Repo.get(topic_id)
+    {:ok, %{}, assign(socket, :topic, topic)}
+  end
+
+  @impl true
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+    changeset = topic
+    |> build_assoc(:comments)
+    |> Comment.changeset(%{content: content})
+
+    changeset
+    |> Repo.insert()
+    |> case do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
     end
-  end
-
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comments:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
+    {:reply, {:ok, content}, socket}
   end
 
   # Add authorization logic here as required.
